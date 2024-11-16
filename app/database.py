0633628d4b06cd502d1ab7cd8d8a4394ba68cb5c
Roberto Argueta <roberto_argueta.1998@hotmail.com>
2024-11-15 18:53:24 -0600

    Version 1.1.3

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Cambia el esquema del URL para usar pymysql
-DATABASE_URL = "mysql+pymysql://root:123@localhost/UES_GASOLINERA"
+DATABASE_URL = "mysql+pymysql://root:CWemXAjScsglxUbimlbHioOhpWaleGTx@junction.proxy.rlwy.net/railway"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
